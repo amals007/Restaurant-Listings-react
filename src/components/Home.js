de1a64effaa59490b85c72113eed46f444d7e83a
Amal S <102399747+amals007@users.noreply.github.com>
2022-07-14 09:48:14 +0530

    fetch data

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useEffect,useState } from "react";
+
+function Home() {
+    const [restaurants, setRestaurants] = useState([])
+    async function fetchData(){
+        await fetch('./restaurants.json')
+        .then((data)=>data.json())
+        .then((res)=> setRestaurants(res.restaurants))
+    }
+    useEffect(() =>{
+        fetchData()
+    },[])
+    console.log('My data is', restaurants);
+  return (
+    <div>
+        {restaurants.map(item=> (
+            <div>
+                <h1>{item.name}</h1>
+            </div>
+        )
+            )}
+    </div>
+  )
+}
+
+export default Home
